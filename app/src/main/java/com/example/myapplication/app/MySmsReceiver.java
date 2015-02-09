97c9a5a36a5b9091e97510fb8bca9f0dd2684d1b
juliencandela <julien.candela@centrale-marseille.fr>
2015-02-09 11:42:05 +0100

    Brodcast Receiver 2 : afficher un Toast à la reception d'un SMS

--- app/src/main/java/com/example/myapplication/app/MySmsReceiver.java
+++ app/src/main/java/com/example/myapplication/app/MySmsReceiver.java
@@ -3,17 +3,16 @@ package com.example.myapplication.app;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.widget.Toast;
 
 /**
  * Created by marie-helene on 09/02/15.
  */
-public class MyReceiver extends BroadcastReceiver {
-
+public class MySmsReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Intent service = new Intent(context, UpdaterService.class);
-        context.startService(service);
+        Toast.makeText(context, "Message nouveau reçu tu as", Toast.LENGTH_LONG).show();
+
     }
 }
-
