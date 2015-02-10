e24af71a830c5bd2823516091a55ca0e654e09c7
juliencandela <julien.candela@centrale-marseille.fr>
2015-02-10 17:03:10 +0100

    CustomAdapter : feue la bambouseraie optimisée

--- app/src/main/java/com/example/myapplication/app/LivreAdapter.java
+++ app/src/main/java/com/example/myapplication/app/LivreAdapter.java
@@ -1,6 +1,7 @@
 package com.example.myapplication.app;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,47 +19,65 @@ public class LivreAdapter extends BaseAdapter {
     // LayoutInflater aura pour mission de charger notre fichier XML
     LayoutInflater inflater;
 
+    /**
+     * Elle nous servira à mémoriser les éléments de la liste en mémoire pour
+     * qu’à chaque rafraichissement l’écran ne scintille pas
+     *
+     * @author patrice
+     */
+    private class ViewHolder {
+        TextView tvTitre;
+        TextView tvAuteur;
+    }
     public LivreAdapter(Context context, List<Livre> objects) {
         inflater = LayoutInflater.from(context);
         this.biblio = objects;
     }
-
     /**
      * Génère la vue pour un objet
      */
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        convertView = inflater.inflate(R.layout.livre_item, null);
-        TextView tvTitre = (TextView) convertView.findViewById(R.id.txtTitre);
-        TextView tvAuteur = (TextView) convertView.findViewById(R.id.txtAuteur);
+        ViewHolder holder;
+        if (convertView == null) {
+            Log.v("test", "convertView is null");
+            holder = new ViewHolder();
+            convertView = inflater.inflate(R.layout.livre_item, null);
+            holder.tvTitre = (TextView) convertView.findViewById(R.id.txtTitre);
+            holder.tvAuteur = (TextView) convertView
+                    .findViewById(R.id.txtAuteur);
+            convertView.setTag(holder);
+        } else {
+            Log.v("test", "convertView is not null");
+            holder = (ViewHolder) convertView.getTag();
+        }
         Livre livre = biblio.get(position);
-        tvTitre.setText(livre.getTitre());
-        tvAuteur.setText(livre.getAuteur());
+        holder.tvTitre.setText(livre.getTitre());
+        holder.tvAuteur.setText(livre.getAuteur());
         return convertView;
     }
-
     /**
      * Retourne le nombre d'éléments
      */
     @Override
     public int getCount() {
+// TODO Auto-generated method stub
         return biblio.size();
     }
-
     /**
      * Retourne l'item à la position
      */
     @Override
     public Livre getItem(int position) {
+// TODO Auto-generated method stub
         return biblio.get(position);
     }
-
     /**
      * Retourne la position de l'item
      */
     @Override
     public long getItemId(int position) {
+// TODO Auto-generated method stub
         return position;
     }
-
 }
